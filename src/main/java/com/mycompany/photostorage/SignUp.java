e1e00b9e4cc0721fe5b36f2c16b9f7041f6688d6
Jakub Widłak (jakuwid792) <jakuwid792@student.polsl.pl>
2017-04-23 00:07:04 +0200

    Połączenie panelów + przechodzenie pomiędzy

--- src/main/java/com/mycompany/photostorage/SignUp.java
+++ src/main/java/com/mycompany/photostorage/SignUp.java
@@ -114,7 +114,7 @@ MainProgramFrame frame;
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         frame.displayMenu();
-        frame.setPanel(new AddCategoryPanel());
+        frame.setPanel(new PhotoViewPanel());
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
